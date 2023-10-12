2510c02f5430622941a51d6883cc2e0d9bcc74ac
Danilo A. Santos <danilo_as1992@hotmail.com>
2023-10-12 12:31:48 -0300

    add user repository

--- src/main/java/br/com/daniloalves/todolist/TodolistApplication.java
+++ src/main/java/br/com/daniloalves/todolist/TodolistApplication.java
@@ -5,10 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodolistApplication {
-
-	//https://start.spring.io/
-	//Para executar clicar em "run" em cima do método main
-	//Abrir console do banco de dados http://localhost:8080/h2-console
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
