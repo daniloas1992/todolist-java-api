4e0ea2ade1b1d28a9f2913b951d3b0acf3fa6346
Danilo A. Santos <danilo_as1992@hotmail.com>
2023-10-12 11:48:46 -0300

    init project

---
+++ src/main/java/br/com/daniloalves/todolist/TodolistApplication.java
@@ -0,0 +1,16 @@
+package br.com.daniloalves.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistApplication {
+
+	//https://start.spring.io/
+	//Para executar clicar em "run" em cima do método main
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistApplication.class, args);
+	}
+
+}
