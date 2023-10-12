53b32fd341d02c3e57cba354908b6713b22c3f63
Danilo A. Santos <danilo_as1992@hotmail.com>
2023-10-12 12:40:49 -0300

    validate user

--- src/main/java/br/com/daniloalves/todolist/user/UserController.java
+++ src/main/java/br/com/daniloalves/todolist/user/UserController.java
@@ -15,6 +15,15 @@ public class UserController {
 
     @PostMapping("/")
     public UserModel create(@RequestBody UserModel userModel) {
+
+        //Verifica se o username já existe
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null) {
+            System.out.println("O username já existe no banco de dados!");
+            return null;
+        }
+
         var userCreated = this.userRepository.save(userModel);
         return userCreated;
     }
