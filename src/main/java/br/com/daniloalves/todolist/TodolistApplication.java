e09eb7500e0effec87e50d982c59e89650615d06
Danilo A. Santos <danilo_as1992@hotmail.com>
2023-10-12 12:11:22 -0300

    config database connection

--- src/main/java/br/com/daniloalves/todolist/TodolistApplication.java
+++ src/main/java/br/com/daniloalves/todolist/TodolistApplication.java
@@ -8,6 +8,7 @@ public class TodolistApplication {
 
 	//https://start.spring.io/
 	//Para executar clicar em "run" em cima do método main
+	//Abrir console do banco de dados http://localhost:8080/h2-console
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
