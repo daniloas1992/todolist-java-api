696394aebaac1f83a79022020b2f9581bb138e25
Danilo A. Santos <danilo_as1992@hotmail.com>
2023-10-12 12:19:59 -0300

    configure model database

--- src/main/java/br/com/daniloalves/todolist/user/UserModel.java
+++ src/main/java/br/com/daniloalves/todolist/user/UserModel.java
@@ -2,11 +2,28 @@ package br.com.daniloalves.todolist.user;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 @Data //Gera os getters e setters de todos atributos
+@Entity(name="tb_users") //Mapear em uma tabela no banco
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
     public String username;
     public String name;
-    public String password; 
+    public String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
