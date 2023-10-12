a58aa2460c82e62f7e011568f795629519a4b8ea
Danilo A. Santos <danilo_as1992@hotmail.com>
2023-10-12 11:55:05 -0300

    add lombok

--- src/main/java/br/com/daniloalves/todolist/user/UserModel.java
+++ src/main/java/br/com/daniloalves/todolist/user/UserModel.java
@@ -1,35 +1,12 @@
 package br.com.daniloalves.todolist.user;
 
+import lombok.Data;
+
+@Data //Gera os getters e setters de todos atributos
 public class UserModel {
 
     public String username;
     public String name;
-    public String password;
+    public String password; 
 
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    
 }
