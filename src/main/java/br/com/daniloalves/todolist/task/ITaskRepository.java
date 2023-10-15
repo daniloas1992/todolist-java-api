0ddacf36a63a84f7f0878bb52f21730d322fd50a
Danilo Alves <danilo_alves@acaciaconsultoria.com.br>
2023-10-15 11:20:13 -0300

    update

--- src/main/java/br/com/daniloalves/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/daniloalves/todolist/task/ITaskRepository.java
@@ -8,5 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
 
   List<TaskModel> findByIdUser(UUID idUser);
+
+  
   
 }
